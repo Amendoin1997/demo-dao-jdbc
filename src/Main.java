765055d6e80adac6422524d735e97370333b7f54
Amendoin1997 <lucaspariziani@gmail.com>
2024-07-31 11:13:19 -0300

    Implementando findAll

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,12 @@ public class Main {
             System.out.println(obj);
         }
 
+        System.out.println("=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
     }
 
 
