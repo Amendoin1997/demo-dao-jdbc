9c8f306e7e32918a11253068001b1750ae6d8190
Amendoin1997 <lucaspariziani@gmail.com>
2024-07-31 09:30:19 -0300

    Department entity class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import Entities.Department;
+import Entities.Seller;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,6 +9,10 @@ public class Main {
     public static void main(String[] args) {
 
         Department department = new Department(1, "books");
+
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, department);
+
+        System.out.println(seller);
         System.out.println(department);
     }
 
