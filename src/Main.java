e8b662252207384747f48bca2977263a1c3c6c34
Amendoin1997 <lucaspariziani@gmail.com>
2024-07-31 11:37:13 -0300

    Implementando delete

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,14 @@ import model.dao.SellerDao;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
@@ -41,6 +44,18 @@ public class Main {
         seller.setName("Martha Wayne");
         sellerDao.update(seller);
         System.out.println("Update completed!");
+
+        System.out.println("=== TEST 6: seller delete ===");
+
+        System.out.println("Enter id for seller delete");
+
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Deleted Id: " + id);
+
+
+        sc.close();
+
 
     }
 
