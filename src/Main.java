0e4f12398c97ca7f13de245ffe71918061fd3fb7
Amendoin1997 <lucaspariziani@gmail.com>
2024-07-31 09:39:12 -0300

    DepartmentDao & SellerDao

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,12 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Department department = new Department(1, "books");
+        Department obj = new Department(1, "books");
 
-        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, department);
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
 
         System.out.println(seller);
-        System.out.println(department);
+        System.out.println(obj);
     }
 
 
